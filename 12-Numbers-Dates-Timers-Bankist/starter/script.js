be5c1a100a0f7c3cc5a403341ee2ced3b7541c8a
Clinton <clintonleex@gmail.com>
2022-02-02 23:03:00 +0100

    bigint and numeric separator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -316,3 +316,25 @@ console.log((2.7).toFixed(0));
 console.log((2.758264).toFixed(3));
 console.log((2.74865).toFixed(2));
 console.log(+(2.74865).toFixed(2));
+
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+
+console.log(6 % 2);
+console.log(7 % 2);
+
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(9));
+
+// labelBalance.addEventListener('click', function () {
+//   [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+//     if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+//     if (i % 3 === 0) row.style.backgroundColor = 'blue';
+//   });
+// });
+
+const diameter = 287_460_000_000;
+console.log(diameter);
